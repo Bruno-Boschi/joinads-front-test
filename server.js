6a8d9d9f32f93f614b7fb1ca64bd59f414bb2a77
Bruno-Boschi <brunoboschi72@gmail.com>
2024-12-09 11:59:28 -0300

    iniciado com node.js

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Curso de express api",
-  "/livros": "Entrei na rotas livros",
-  "/autores": "Entrei na rotas autores",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/pain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
